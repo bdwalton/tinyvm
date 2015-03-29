c9da4d88129b00cd0c4ded3165a197efcd57b9de
Ben Walton <bdwalton@gmail.com>
2015-03-29 21:13:37 +0100

    initial commits of tinyvm*
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

---
+++ tinyvm.go
@@ -0,0 +1,465 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const MEM_SIZE = 1024 // How much data memory? (Used to size both data and program memory.)
+const NUM_REGS = 8    // The total number of registers available.
+const PC_REG = 7      // The registered used as the program counter.
+
+// Instructions are composed of one operation and up to three
+// arguments.
+type TinyInstruction struct {
+	iop   string
+	iargs []int
+}
+
+/* A structure representing a tiny machine */
+type TinyMachine struct {
+	stdin              *bufio.Reader             // To handle data input
+	registers          [NUM_REGS]int             // 8 registers
+	data_memory        [MEM_SIZE]int             // Data memory
+	instruction_memory [MEM_SIZE]TinyInstruction // Instruction memory
+	trace              bool                      // Output instructions as they're executed
+	halted             bool                      // True after executing a HALT instruction
+}
+
+// Operands are of the form r,s,t where r, s and t are all integers
+func parseROop(args string) ([]int, error) {
+	string_args := strings.Split(args, ",")
+	converted_args := make([]int, 3)
+
+	if len(string_args) != 3 {
+		return nil, errors.New("Invalid arguments: " + args)
+	} else {
+		for i := 0; i < 3; i++ {
+			num, err := strconv.Atoi(string_args[i])
+			if err != nil {
+				return nil, errors.New("Invalid arguments: " + args)
+			} else {
+				converted_args[i] = num
+			}
+		}
+	}
+
+	return converted_args, nil
+}
+
+// Operands are of the form r,s(t) where r, s and t are all integers
+func parseRMop(args string) ([]int, error) {
+	converted_args := make([]int, 3)
+
+	x := strings.Index(args, ",")
+	y := strings.Index(args, "(")
+	z := strings.Index(args, ")")
+
+	if x < 1 || y < x || z < y {
+		return nil, errors.New("Invalid arguments: " + args)
+	} else {
+		indexes := [][]int{[]int{0, x}, []int{x + 1, y}, []int{y + 1, z}}
+
+		for i, bounds := range indexes {
+			num, err := strconv.Atoi(args[bounds[0]:bounds[1]])
+			if err != nil {
+				return nil, errors.New("Invalid arguments: " + args)
+			} else {
+				converted_args[i] = num
+			}
+		}
+	}
+
+	return converted_args, nil
+}
+
+func parseInstruction(line string) (TinyInstruction, error) {
+	var args []int
+	var err error
+	var ti TinyInstruction
+
+	// Chop the newline off and then split on spaces
+	r := regexp.MustCompile(" +")
+	stripped_line := strings.TrimSpace(r.ReplaceAllString(line, " "))
+	line_parts := strings.Split(stripped_line, " ")
+
+	if len(line_parts) != 2 {
+		return ti, errors.New("Invalid instruction: '" + stripped_line + "'")
+	} else {
+		switch line_parts[0] {
+		case "HALT", "IN", "OUT", "ADD", "SUB", "MUL", "DIV":
+			args, err = parseROop(line_parts[1])
+		case "LD", "ST", "LDA", "LDC", "JLT", "JLE", "JGT", "JGE", "JEQ", "JNE":
+			args, err = parseRMop(line_parts[1])
+		default:
+			return ti, errors.New("Invalid opcode: '" + line_parts[0] + "'")
+		}
+
+		if err != nil {
+			m := "Invalid arguments for opcode " + line_parts[0] + ": '" + line_parts[1] + "'"
+			return ti, errors.New(m)
+		} else {
+			ti.iop = line_parts[0]
+			ti.iargs = args
+		}
+	}
+
+	return ti, nil
+}
+
+func (tm *TinyMachine) initializeMachine(clearprogram bool) {
+	for i := 0; i < NUM_REGS; i++ {
+		tm.registers[i] = 0
+	}
+
+	for i := 0; i < MEM_SIZE; i++ {
+		tm.data_memory[i] = 0
+	}
+
+	if clearprogram {
+		for i := 0; i < MEM_SIZE; i++ {
+			tm.instruction_memory[i] = TinyInstruction{"HALT", []int{0, 0, 0}}
+		}
+	}
+
+	// Store the size of the memory in the first memory element.
+	tm.data_memory[0] = MEM_SIZE - 1
+	tm.halted = false
+	tm.registers[PC_REG] = 0
+	tm.stdin = bufio.NewReader(os.Stdin) // An io helper.
+}
+
+// Leave the loaded program intact, but re-initialize the machine to a
+// clean state otherwise.
+func (tm *TinyMachine) resetState() {
+	// Reset memory and registers, but leave program intact.
+	tm.initializeMachine(false)
+}
+
+func (tm *TinyMachine) stepProgram() (err error) {
+	pc := tm.registers[PC_REG]
+	if pc < 0 || pc > MEM_SIZE {
+		return errors.New("Out of bounds instruction memory access.")
+	} else {
+		// Step the program counter
+		tm.registers[PC_REG] = pc + 1
+
+		instruction := tm.instruction_memory[pc]
+		if tm.trace {
+			fmt.Println("Executing:", instruction)
+		}
+
+		switch instruction.iop {
+		case "HALT":
+			tm.halted = true
+			return nil
+		case "IN":
+			reg := instruction.iargs[0]
+			m := fmt.Sprintf("Enter number to store in register %d", reg)
+			for {
+				n := tm.readNumber(m, 0)
+				if err != nil {
+					fmt.Println("Error reading input. Try again.")
+				} else {
+					tm.registers[reg] = n
+					break
+				}
+			}
+		case "OUT":
+			fmt.Println(tm.registers[instruction.iargs[0]])
+		case "ADD":
+			r := instruction.iargs[0]
+			s := instruction.iargs[1]
+			t := instruction.iargs[2]
+			tm.registers[r] = tm.registers[s] + tm.registers[t]
+		case "SUB":
+			r := instruction.iargs[0]
+			s := instruction.iargs[1]
+			t := instruction.iargs[2]
+			tm.registers[r] = tm.registers[s] - tm.registers[t]
+		case "MUL":
+			r := instruction.iargs[0]
+			s := instruction.iargs[1]
+			t := instruction.iargs[2]
+			tm.registers[r] = tm.registers[s] * tm.registers[t]
+		case "DIV":
+			r := instruction.iargs[0]
+			s := instruction.iargs[1]
+			t := instruction.iargs[2]
+			if tm.registers[t] == 0 {
+				return errors.New("Divide by zero!")
+				tm.halted = true
+			} else {
+				tm.registers[r] = tm.registers[s] / tm.registers[t]
+			}
+		case "LDA":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if a < 0 || a > MEM_SIZE {
+				return errors.New("Out of bounds memory access.")
+			} else {
+				tm.registers[instruction.iargs[0]] = a
+			}
+		case "LDC":
+			tm.registers[instruction.iargs[0]] = instruction.iargs[1]
+		case "LD":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if a < 0 || a > MEM_SIZE {
+				return errors.New("Out of bounds memory access.")
+			} else {
+				tm.registers[instruction.iargs[0]] = tm.data_memory[a]
+			}
+		case "ST":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if a < 0 || a > MEM_SIZE {
+				return errors.New("Out of bounds memory access.")
+			} else {
+				tm.data_memory[a] = tm.registers[instruction.iargs[0]]
+			}
+		case "JLT":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if tm.registers[instruction.iargs[0]] < 0 {
+				tm.registers[PC_REG] = a
+			}
+		case "JLE":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if tm.registers[instruction.iargs[0]] <= 0 {
+				tm.registers[PC_REG] = a
+			}
+		case "JGE":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if tm.registers[instruction.iargs[0]] >= 0 {
+				tm.registers[PC_REG] = a
+			}
+		case "JGT":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if tm.registers[instruction.iargs[0]] > 0 {
+				tm.registers[PC_REG] = a
+			}
+		case "JEQ":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if tm.registers[instruction.iargs[0]] == 0 {
+				tm.registers[PC_REG] = a
+			}
+		case "JNE":
+			d := instruction.iargs[1]
+			a := d + tm.registers[instruction.iargs[2]]
+			if tm.registers[instruction.iargs[0]] != 0 {
+				tm.registers[PC_REG] = a
+			}
+		}
+	}
+
+	return nil
+}
+
+func (tm *TinyMachine) runProgram() {
+	for {
+		if tm.halted {
+			break
+		} else {
+			tm.stepProgram()
+		}
+	}
+}
+
+func (tm *TinyMachine) loadProgram(progfilename string) bool {
+	var (
+		i       int
+		linenum int = 0
+	)
+
+	tm.initializeMachine(true)
+
+	programfile, err := os.Open(progfilename)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return false
+	} else {
+		defer programfile.Close()
+		reader := bufio.NewReader(programfile)
+		fmt.Println("Reading program from", progfilename)
+
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				} else {
+					fmt.Println("Error reading program:", err)
+					return false
+				}
+			} else {
+				linenum++
+				if strings.Index(line, "*") == 0 {
+					// Comments are lines starting with an asterisk
+					continue
+				} else {
+					// TODO(bdwalton): Skip over blank lines.
+					instruction, err := parseInstruction(line[:len(line)-1])
+
+					if err != nil {
+						fmt.Println(err)
+						fmt.Printf("Error parsing program at line %d: %s\n", linenum, line)
+						return false
+					} else {
+						tm.instruction_memory[i], i = instruction, i+1
+					}
+				}
+			}
+		}
+	}
+
+	return true
+}
+
+func (tm *TinyMachine) dumpRegisters() {
+	fmt.Println("Current Tiny Machine register values:")
+
+	for i := 0; i < NUM_REGS; i++ {
+		switch i {
+		case PC_REG:
+			fmt.Println("PC:", tm.registers[i])
+		default:
+			fmt.Printf("%2d: %d\n", i, tm.registers[i])
+		}
+	}
+}
+
+func (tm *TinyMachine) dumpMemory(start_addr, end_addr int) {
+	fmt.Println("Dumping data memory from address %d to %d", start_addr, end_addr)
+
+	for i := start_addr; i <= end_addr; i++ {
+		fmt.Printf("%04d: %d\n", i, tm.data_memory[i])
+	}
+}
+
+func (tm *TinyMachine) dumpProgram(start_addr, end_addr int) {
+	fmt.Printf("Dumping instruction memory from address %d to %d.\n", start_addr, end_addr)
+
+	for i := start_addr; i <= end_addr; i++ {
+		fmt.Printf("%04d: %v\n", i, tm.instruction_memory[i])
+	}
+}
+
+func (tm *TinyMachine) readNumber(prompt string, def int) int {
+	for {
+		fmt.Printf("%s: ", prompt)
+		input, err := tm.stdin.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input. Returning default", def)
+			break
+		} else {
+			num, err := strconv.Atoi(input[:len(input)-1])
+			if err != nil {
+				fmt.Println("Error converting input. Returning default", def)
+				break
+			} else {
+				return num
+			}
+		}
+	}
+
+	return def
+}
+
+func (tm *TinyMachine) Interact() {
+	menu := []struct {
+		key      string // The key to press to activate this option.
+		helptext string // The help text displayed
+	}{
+		{"c", "clear machine state"},
+		{"g", "go - run program to halt state"},
+		{"h", "display this help text"},
+		{"d", "display data memory"},
+		{"i", "display instruction memory"},
+		{"q", "quit the tiny machine simulator"},
+		{"r", "dump register contents"},
+		{"s", "step program forward by one instruction"},
+		{"t", "toggle execution tracing"},
+	}
+
+	fmt.Println("Tiny Machine simulation (enter h for help)")
+interactive:
+	for {
+		fmt.Printf("Enter command: ")
+		input, _ := tm.stdin.ReadString('\n')
+		command := input[:len(input)-1]
+
+		switch command {
+		case "c":
+			tm.resetState()
+		case "g":
+			tm.runProgram()
+		case "h":
+			for _, menuitem := range menu {
+				fmt.Printf("%s: %s\n", menuitem.key, menuitem.helptext)
+			}
+		case "d":
+			start_addr := tm.readNumber("Starting Address", 0)
+			end_addr := tm.readNumber("Ending Address", MEM_SIZE-1)
+			if start_addr > end_addr || start_addr < 0 {
+				fmt.Println("Invalid memory region.")
+			}
+
+			if end_addr >= MEM_SIZE {
+				fmt.Println("Invalid memory region.")
+			} else {
+				tm.dumpMemory(start_addr, end_addr)
+			}
+		case "i":
+			start_addr := tm.readNumber("Starting Address", 0)
+			end_addr := tm.readNumber("Ending Address", MEM_SIZE-1)
+			if start_addr > end_addr || start_addr < 0 {
+				fmt.Println("Invalid memory region.")
+			}
+
+			if end_addr >= MEM_SIZE {
+				fmt.Println("Invalid memory region.")
+			} else {
+				tm.dumpProgram(start_addr, end_addr)
+			}
+		case "q":
+			fmt.Println("Exiting.")
+			break interactive
+		case "r":
+			tm.dumpRegisters()
+		case "s":
+			tm.stepProgram()
+		case "t":
+			tm.trace = !tm.trace
+			fmt.Println("Execution tracing is now", tm.trace)
+		default:
+			fmt.Println("Not implemented yet. Try 'h' for help.")
+		}
+	}
+
+}
+
+func main() {
+	var tm TinyMachine
+
+	if len(os.Args) < 2 {
+		fmt.Println("You must supply a program as the first argument.")
+	} else {
+		if tm.loadProgram(os.Args[1]) {
+			tm.Interact()
+		} else {
+			fmt.Println("Error loading program from:", os.Args[1])
+		}
+	}
+}
---
+++ tinyvm_test.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseRMop(t *testing.T) {
+	cases := []struct {
+		in       string
+		want     []int
+		want_err string
+	}{
+		{"0,0(1)", []int{0, 0, 1}, ""},
+		{"10,10(10)", []int{10, 10, 10}, ""},
+		{"10,a(1)", nil, "Invalid arguments: 10,a(1)"},
+		{"a,10(1)", nil, "Invalid arguments: a,10(1)"},
+		{",10(1)", nil, "Invalid arguments: ,10(1)"},
+		{"1,(1)", nil, "Invalid arguments: 1,(1)"},
+		{"1,", nil, "Invalid arguments: 1,"},
+		{"1", nil, "Invalid arguments: 1"},
+		{"", nil, "Invalid arguments: "},
+	}
+	for _, c := range cases {
+		got, got_err := parseRMop(c.in)
+		if got_err != nil {
+			if c.want_err == "" {
+				t.Errorf("Unexpected error raised for parseRMop(%q): %q", c.in, got_err.Error())
+			} else if c.want_err != got_err.Error() {
+				t.Errorf("Expected error '%q' but got '%q'.", c.want_err, got_err.Error())
+			}
+		}
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("parseRMop(%q) == %q, want %q.", c.in, got, c.want)
+		}
+	}
+}
+
+func TestParseROop(t *testing.T) {
+	cases := []struct {
+		in       string
+		want     []int
+		want_err string
+	}{
+		{"0,0,1", []int{0, 0, 1}, ""},
+		{"10,10,10", []int{10, 10, 10}, ""},
+		{"10,a,1", nil, "Invalid arguments: 10,a,1"},
+		{"a,10,1", nil, "Invalid arguments: a,10,1"},
+		{",10,1", nil, "Invalid arguments: ,10,1"},
+		{"1,,1", nil, "Invalid arguments: 1,,1"},
+		{"1,", nil, "Invalid arguments: 1,"},
+		{"1", nil, "Invalid arguments: 1"},
+		{"", nil, "Invalid arguments: "},
+	}
+	for _, c := range cases {
+		got, got_err := parseROop(c.in)
+		if got_err != nil {
+			if c.want_err == "" {
+				t.Errorf("Unexpected error raised for parseROop(%q): %q", c.in, got_err.Error())
+			} else if c.want_err != got_err.Error() {
+				t.Errorf("Expected error '%q' but got '%q'", c.want_err, got_err.Error())
+			}
+		}
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("parseROop(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func TestParseInstruction(t *testing.T) {
+	cases := []struct {
+		in       string
+		want     TinyInstruction
+		want_err string
+	}{
+		// Valid RO instructions
+		{"HALT   0,0,1", TinyInstruction{"HALT", []int{0, 0, 1}}, ""},
+		{"IN     0,0,1", TinyInstruction{"IN", []int{0, 0, 1}}, ""},
+		{"OUT    0,0,0", TinyInstruction{"OUT", []int{0, 0, 0}}, ""},
+		{"ADD    0,0,0", TinyInstruction{"ADD", []int{0, 0, 0}}, ""},
+		{"SUB    0,0,0", TinyInstruction{"SUB", []int{0, 0, 0}}, ""},
+		{"MUL    0,0,0", TinyInstruction{"MUL", []int{0, 0, 0}}, ""},
+		{"DIV    0,0,0", TinyInstruction{"DIV", []int{0, 0, 0}}, ""},
+		// Valid RM instructions
+		{"LD     0,0(0)", TinyInstruction{"LD", []int{0, 0, 0}}, ""},
+		{"ST     0,0(0)", TinyInstruction{"ST", []int{0, 0, 0}}, ""},
+		{"LDA    0,0(0)", TinyInstruction{"LDA", []int{0, 0, 0}}, ""},
+		{"LDC    0,0(0)", TinyInstruction{"LDC", []int{0, 0, 0}}, ""},
+		{"JLT    0,0(0)", TinyInstruction{"JLT", []int{0, 0, 0}}, ""},
+		{"JLE    0,0(0)", TinyInstruction{"JLE", []int{0, 0, 0}}, ""},
+		{"JGT    0,0(0)", TinyInstruction{"JGT", []int{0, 0, 0}}, ""},
+		{"JGE    0,0(0)", TinyInstruction{"JGE", []int{0, 0, 0}}, ""},
+		{"JEQ    0,0(0)", TinyInstruction{"JEQ", []int{0, 0, 0}}, ""},
+		{"JNE    0,0(0)", TinyInstruction{"JNE", []int{0, 0, 0}}, ""},
+		// Garbage spaces are handled properly
+		{"   HALT  0,0,1   ", TinyInstruction{"HALT", []int{0, 0, 1}}, ""},
+		{"   LD  0,0(1)   ", TinyInstruction{"LD", []int{0, 0, 1}}, ""},
+		// RM format for RO opcode
+		{"IN    0,0(1)", TinyInstruction{}, "Invalid arguments for opcode IN: '0,0(1)'"},
+		// RO format for RM opcode
+		{"LD    0,0,0", TinyInstruction{}, "Invalid arguments for opcode LD: '0,0,0'"},
+		// Missing opcode
+		{"   0,0,1   ", TinyInstruction{}, "Invalid instruction: '0,0,1'"},
+		{"   0,0(1)   ", TinyInstruction{}, "Invalid instruction: '0,0(1)'"},
+		// Missing operands
+		{"OPCODE   ", TinyInstruction{}, "Invalid instruction: 'OPCODE'"},
+		// Invalid opcode
+		{"OPCODE 0,0,1   ", TinyInstruction{}, "Invalid opcode: 'OPCODE'"},
+		{"OPCODE 0,0(1)  ", TinyInstruction{}, "Invalid opcode: 'OPCODE'"},
+		// Garbage inputs
+		{"IN 0,a,1   ", TinyInstruction{}, "Invalid arguments for opcode IN: '0,a,1'"},
+		{"ST 0,a(1)   ", TinyInstruction{}, "Invalid arguments for opcode ST: '0,a(1)'"},
+	}
+	for _, c := range cases {
+		got, got_err := parseInstruction(c.in)
+		if got_err != nil {
+			if c.want_err == "" {
+				t.Errorf("Unexpected error raised for parseInstruction(%q): %q.", c.in, got_err.Error())
+			} else if c.want_err != got_err.Error() {
+				t.Errorf("Expected error '%q' but got '%q'.", c.want_err, got_err.Error())
+			}
+		}
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("parseInstruction(%q) == %q, want %q.", c.in, got, c.want)
+		}
+	}
+}
+
+func TestResetState(t *testing.T) {
+	var tm TinyMachine
+
+	tm.halted = true
+	tm.instruction_memory[0] = TinyInstruction{"LDC", []int{1, 1, 1}}
+	tm.instruction_memory[MEM_SIZE-1] = TinyInstruction{"ADD", []int{1, 1, 1}}
+	tm.data_memory[0] = 1
+	tm.data_memory[MEM_SIZE-1] = 100
+	tm.registers[PC_REG] = 1
+
+	tm.resetState()
+
+	if tm.halted {
+		t.Errorf("Resetting machine didn't clear halt state.")
+	} else if !reflect.DeepEqual(TinyInstruction{"LDC", []int{1, 1, 1}},
+		tm.instruction_memory[0]) {
+		t.Errorf("Resetting machine cleared instructions.")
+	} else if !reflect.DeepEqual(TinyInstruction{"ADD", []int{1, 1, 1}},
+		tm.instruction_memory[MEM_SIZE-1]) {
+		t.Errorf("Resetting machine cleared instructions.")
+	} else if tm.data_memory[0] != MEM_SIZE-1 {
+		t.Errorf("Resetting machine didn't reset memory state.")
+	} else if tm.registers[PC_REG] != 0 {
+		t.Errorf("Initializing machine didn't reset the program counter.")
+	}
+}
+
+func TestInitializeMachine(t *testing.T) {
+	var tm TinyMachine
+
+	tm.halted = true
+	tm.instruction_memory[0] = TinyInstruction{"LDC", []int{1, 1, 1}}
+	tm.instruction_memory[MEM_SIZE-1] = TinyInstruction{"ADD", []int{1, 1, 1}}
+	tm.data_memory[0] = 1
+	tm.data_memory[MEM_SIZE-1] = 100
+	tm.registers[PC_REG] = 1
+
+	tm.initializeMachine(true)
+
+	if tm.halted {
+		t.Errorf("Initializing machine didn't clear halt state.")
+	} else if !reflect.DeepEqual(TinyInstruction{"HALT", []int{0, 0, 0}}, tm.instruction_memory[0]) {
+		t.Errorf("Initializing machine didn't clear instruction memory.")
+	} else if !reflect.DeepEqual(TinyInstruction{"HALT", []int{0, 0, 0}}, tm.instruction_memory[MEM_SIZE-1]) {
+		t.Errorf("Initializing machine didn't clear instruction memory.")
+	} else if tm.data_memory[0] != MEM_SIZE-1 {
+		t.Errorf("Initializing machine didn't reset memory state.")
+	} else if tm.registers[PC_REG] != 0 {
+		t.Errorf("Initializing machine didn't reset the program counter.")
+	}
+}
