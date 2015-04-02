35ea2a83f9c05564b5c70022ed897aa1ccd083a5
Ben Walton <bdwalton@gmail.com>
2015-04-02 07:14:40 +0100

    add tests for the LDC instruction
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

--- tinyvm_test.go
+++ tinyvm_test.go
@@ -420,3 +420,34 @@ func TestSTInstruction(t *testing.T) {
 		}
 	}
 }
+
+func TestLDCInstruction(t *testing.T) {
+	var tm TinyMachine
+
+	tm.initializeMachine(true)
+	// Stuff some values into the registers
+	tm.registers = [NUM_REGS]int{0, 0, 0, 0, 0, 0, 0, 0}
+
+	tm.instruction_memory[0] = TinyInstruction{"LDC", []int{0, 100, 0}, iopRA} // 100 -> reg0
+	tm.instruction_memory[1] = TinyInstruction{"LDC", []int{1, -2, 1}, iopRA}  // -2 -> reg1
+
+	cases := []struct {
+		expected_reg int
+		expected_val int
+		expected_cpu TinyCPUState
+	}{
+		{0, 100, cpuOK},
+		{1, -2, cpuOK},
+	}
+	for _, c := range cases {
+		tm.stepProgram()
+		if tm.registers[c.expected_reg] != c.expected_val {
+			t.Errorf("LDC instruction didn't work. Expected %d in reg[%d]. Got %d.",
+				c.expected_val, c.expected_reg, tm.registers[c.expected_reg])
+		}
+		if tm.cpustate != c.expected_cpu {
+			t.Errorf("LDC instruction fine, but cpuState invalid. Wanted %d, got %d.",
+				c.expected_cpu, tm.cpustate)
+		}
+	}
+}
