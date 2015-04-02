0efa2f05a0a30f58742e7f914c96496ab16f9545
Ben Walton <bdwalton@gmail.com>
2015-04-02 08:20:01 +0100

    add tests for the JNE instruction
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

--- tinyvm_test.go
+++ tinyvm_test.go
@@ -649,3 +649,36 @@ func TestJEQInstruction(t *testing.T) {
 		}
 	}
 }
+
+func TestJNEInstruction(t *testing.T) {
+	var tm TinyMachine
+
+	tm.initializeMachine(true)
+	// Stuff some values into the registers
+	tm.registers = [NUM_REGS]int{1, -145, 0, 1, 0, 0, 0, 0}
+
+	tm.instruction_memory[0] = TinyInstruction{"JNE", []int{0, 100, 2}, iopRA} // pcreg -> 100
+	tm.instruction_memory[2] = TinyInstruction{"JNE", []int{5, 5, 3}, iopRA}   // !(pcreg -> 6)
+	tm.instruction_memory[100] = TinyInstruction{"JNE", []int{1, 1, 0}, iopRA} // pcreg -> 2
+
+	cases := []struct {
+		expected_pc  int          // Expected PC value
+		expected_cpu TinyCPUState // Expected CPU state
+	}{
+		{100, cpuOK},
+		{2, cpuOK},
+		{3, cpuOK},
+		{4, cpuHALTED},
+	}
+	for i, c := range cases {
+		tm.stepProgram()
+		if tm.registers[PC_REG] != c.expected_pc {
+			t.Errorf("%d: JNE instruction didn't work. Expected PC to be %d. Got %d.",
+				i, c.expected_pc, tm.registers[PC_REG])
+		}
+		if tm.cpustate != c.expected_cpu {
+			t.Errorf("%d: JNE instruction fine, but cpuState invalid. Wanted %d, got %d.",
+				i, c.expected_cpu, tm.cpustate)
+		}
+	}
+}
